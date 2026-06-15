e41df985c31e6eaa7d5299b2242390ebd9399ed4
li <stevens8394@gmail.com>
2026-06-15 13:25:48 +0800

    style(wf88): fix NetView layout overlapping and bump app version
    - Add box-sizing: border-box to .wf88-panel textarea to prevent width overflow.
    - Add min-width: 0 to .wf88-view-panel to allow shrinking in flex containers.
    - Add display: block and 100% sizing to #wf88Topology SVG canvas.
    - Bump APP_VERSION to 20260615B to force service worker update.

--- js/core/app_version.js
+++ js/core/app_version.js
@@ -1,4 +1,4 @@
 (function (root) {
     root.TermPWA = root.TermPWA || {};
-    root.TermPWA.APP_VERSION = "20260601A";
+    root.TermPWA.APP_VERSION = "20260615B";
 })(typeof self !== "undefined" ? self : globalThis);
